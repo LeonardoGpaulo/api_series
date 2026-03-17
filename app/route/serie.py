1a2dc3f7fc737485a8f71ba074acd57db0b8fce9
leonardoGpaulo <lgualbertodepaulo@gmail.com>
2026-03-17 10:56:28 -0300

    fix: arrumando bugs do deletar e atualizar

--- app/route/serie.py
+++ app/route/serie.py
@@ -18,23 +18,48 @@ async def criar_serie(dados: SerieSchema, db: Session = Depends(get_db)):
 async def listar_series(db: Session = Depends(get_db)):
     return db.query(SerieModel).all()
 
-@serie.put("/update/")
+# @serie.put("/update/")
+# async def atualizar_serie(id: int, dados: SerieSchema, db: Session = Depends(get_db)):
+#     serie = db.query(SerieModel).filter(SerieModel.id == id).first()
+#     if not serie:
+#         return ("Série não encontrada")
+
+#     db.query(SerieModel).filter(SerieModel.id == id).update(dados.model_dump(exclude_unset=True))
+#     db.commit()
+#     return (dados)
+
+@serie.put("/serie/{id}/update")
 async def atualizar_serie(id: int, dados: SerieSchema, db: Session = Depends(get_db)):
+
+    #busca os dados no banco
     serie = db.query(SerieModel).filter(SerieModel.id == id).first()
+
+    #verifica se existe
     if not serie:
-        return ("Série não encontrada")
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"A série com ID {id} não foi eencontrada"
+        )
+    
+    # Atualiza os campos com os novos dados
+    for campo, valor in dados.model_dump().items():
+        setattr(serie, campo, valor)
 
-    db.query(SerieModel).filter(SerieModel.id == id).update(dados.model_dump(exclude_unset=True))
     db.commit()
-    return (dados)
+    db.refresh(serie)
+    return serie
 
 
-@serie.delete("/deletar/")
+@serie.delete("/serie/{id}/delete")
 async def deletar_series(id:int, db: Session = Depends(get_db)):
     id = db.query(SerieModel).filter(SerieModel.id == id).first()
 
     if not id:
-        return("nao tem ID")
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"A série com ID {id} não foi encontrada"
+        )
+
     db.delete(id)
     db.commit()
     return("deletado")
